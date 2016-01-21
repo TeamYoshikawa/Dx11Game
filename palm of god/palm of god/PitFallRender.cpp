ab2a8a9887fea074c980cd7bc1e14bd63bbbb452
ydakoukn <ydakoukun0105@gmail.com>
2016-01-22 00:14:45 +0900

    いらないのを削除

--- palm of god/palm of god/PitFallRender.cpp
+++
@@ -1,9 +0,0 @@
-#include "PitFallRender.h"
-
-using namespace Pitfall;
-using namespace aetherClass;
-void PitFallRender::Render(ModelBase* model, std::shared_ptr<ShaderBase>shader){
-	model->Render(shader.get());
-
-}
-
--- palm of god/palm of god/RockRender.cpp
+++
@@ -1,9 +0,0 @@
-#include "RockRender.h"
-
-using namespace n_Rock;
-using namespace aetherClass;
-void RockRender::Render(ModelBase* model, std::shared_ptr<ShaderBase>shader){
-	model->Render(shader.get());
-
-	return;
-}
