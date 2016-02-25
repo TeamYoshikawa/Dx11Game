21dbef476c8f9f87bd60107a3fcf87d9cd0b0cf6
maturibayasi <maturibayasi1025@gmail.com>
2016-02-26 03:05:57 +0900

    一度上げ直し

--- palmofgod/palm of god/RockRender.cpp
+++
@@ -1,9 +0,0 @@
-#include "RockRender.h"
-
-using namespace n_Rock;
-using namespace aetherClass;
-void RockRender::Render(ModelBase* model, std::shared_ptr<ShaderBase>shaderBase){
-	model->Render(shaderBase.get());
-
-	
-}
